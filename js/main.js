1bf2d5403289c2753086d5a2c7f29af1941d5aa8
mo3az <51883008+mo3az96@users.noreply.github.com>
2020-07-07 21:37:02 +0200

    arrow

--- js/main.js
+++ js/main.js
@@ -7,6 +7,17 @@ $(window).on('load', function () {
 $(document).ready(function () {
     new WOW().init();
 
+    $(window).scroll(function () {
+        $(this).scrollTop() >= 500 ? $(".arr-top").fadeIn(500) : $(".arr-top").fadeOut(500);
+    });
+
+
+    $(".arr-top").click(function () {
+        $("html,body").animate({
+            scrollTop: 0
+        }, 1500);
+    });
+
     ///////// **main Slider** /////////
     var mainswiper = new Swiper('.main-slider .swiper-container', {
         spaceBetween: 10,
